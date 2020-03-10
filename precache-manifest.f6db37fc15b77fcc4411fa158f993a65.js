e31370a959751a8b41aa7a63043e21f38681e3dd
abdullahshaikh135 <abdullahshaikhab@gmail.com>
2020-03-10 20:55:08 +0530

    Updates

---
+++ precache-manifest.f6db37fc15b77fcc4411fa158f993a65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f23eae4054041157c389e70890657c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22ae14240aa4c0c65e67",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a1d0df85f0551c9d1c1a",
+    "url": "/static/css/main.f940ce53.chunk.css"
+  },
+  {
+    "revision": "22ae14240aa4c0c65e67",
+    "url": "/static/js/2.868893b6.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/static/js/2.868893b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d0df85f0551c9d1c1a",
+    "url": "/static/js/main.658e57c8.chunk.js"
+  },
+  {
+    "revision": "dbb9b27142ad05448616",
+    "url": "/static/js/runtime-main.cc45d4ef.js"
+  },
+  {
+    "revision": "ab88ab80686a927b3016225fcfc72e4b",
+    "url": "/static/media/mypic.ab88ab80.jpg"
+  }
+]);
